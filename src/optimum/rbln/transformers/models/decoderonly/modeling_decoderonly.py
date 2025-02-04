c2a3383aa64453afb3db9046af6be68e28bbd8c5
thkim <thkim@rebellions.ai>
2025-02-04 12:24:57 +0900

    minor fix

--- src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
+++ src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
@@ -299,7 +299,7 @@ class RBLNDecoderOnlyModelForCausalLM(RBLNModel):
         rbln_kvcache_partition_len = rbln_kwargs.get("kvcache_partition_len", None)
         rbln_quantization = QuantizationManager.validate_quantization_config(rbln_kwargs.get("quantization", None))
 
-        rbln_prefill_chunk_size = rbln_kwargs.get("prefill_chunk_size")
+        rbln_prefill_chunk_size = rbln_kwargs.get("prefill_chunk_size",128)
 
         if rbln_prefill_chunk_size is None:
             warnings.warn("prefill_chunk_size is None. Falling back to default value: 128.", UserWarning)
