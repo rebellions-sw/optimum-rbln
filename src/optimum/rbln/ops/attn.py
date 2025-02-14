be74f121c1923da43b425eec28e92650413ade6e
rebel-thkim <157466331+rebel-thkim@users.noreply.github.com>
2025-02-14 15:06:11 +0900

    model: batch attention decode(seq2seq) (#24)

--- src/optimum/rbln/ops/attn.py
+++ src/optimum/rbln/ops/attn.py
@@ -152,16 +152,16 @@ def register_rbln_custom_attention_add_softmax():
         """
         return (
             q,
-            torch.empty(1, *kcache.shape[1:], device=kcache.device),
-            torch.empty(1, *vcache.shape[1:], device=vcache.device),
+            torch.empty(*kcache.shape, device=kcache.device),
+            torch.empty(*vcache.shape, device=vcache.device),
         )
 
     @register_fake("rbln_custom_ops::attn_decode_add_softmax")
     def attn_decode_add_softmax_abstract(q, k, v, m, kcache, vcache, seq, partition):
         return (
             q,
-            torch.empty(1, *kcache.shape[1:], device=kcache.device),
-            torch.empty(1, *vcache.shape[1:], device=vcache.device),
+            torch.empty(*kcache.shape, device=kcache.device),
+            torch.empty(*vcache.shape, device=vcache.device),
         )
 
     torch.library.define(
