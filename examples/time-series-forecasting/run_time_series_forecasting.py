9119e0a523b40ec79e23b1b3cacce55680a77d9c
thkim <thkim@rebellions.ai>
2025-02-25 15:33:58 +0900

    tmp run

--- examples/time-series-forecasting/run_time_series_forecasting.py
+++ examples/time-series-forecasting/run_time_series_forecasting.py
@@ -4,7 +4,6 @@ from transformers import TimeSeriesTransformerForPrediction
 
 from optimum.rbln import RBLNTimeSeriesTransformerForPrediction
 
-rbln_model = RBLNTimeSeriesTransformerForPrediction.from_pretrained("huggingface/time-series-transformer-tourism-monthly",export=True)
 
 # 데이터 다운로드 및 로드
 file = hf_hub_download(
@@ -12,10 +11,9 @@ file = hf_hub_download(
 )
 batch = torch.load(file)
 
+rbln_model = RBLNTimeSeriesTransformerForPrediction.from_pretrained("huggingface/time-series-transformer-tourism-monthly",export=True)
 # 사전 학습된 Time Series Transformer 모델 로드
 model = TimeSeriesTransformerForPrediction.from_pretrained("huggingface/time-series-transformer-tourism-monthly")
-
-
 
 # 예측 수행
 outputs = model.generate(
@@ -32,21 +30,21 @@ mean_prediction = outputs.sequences.mean(dim=1)
 
 print(mean_prediction)
 
-past_observed_mask = torch.nn.functional.pad(batch["past_observed_mask"], (0, 3))
-past_values = torch.nn.functional.pad(batch["past_values"], (0, 3))
-past_time_features = torch.nn.functional.pad(batch["past_time_features"], (0, 0, 0, 3))
+# past_observed_mask = torch.nn.functional.pad(batch["past_observed_mask"], (0, 3))
+# past_values = torch.nn.functional.pad(batch["past_values"], (0, 3))
+# past_time_features = torch.nn.functional.pad(batch["past_time_features"], (0, 0, 0, 3))
 
-outputs = model.generate(
-    past_values=past_values,
-    past_time_features=past_time_features,
-    past_observed_mask=past_observed_mask,
-    static_categorical_features=batch["static_categorical_features"],
-    static_real_features=batch["static_real_features"],
-    future_time_features=batch["future_time_features"],
-)
+# outputs = model.generate(
+#     past_values=past_values,
+#     past_time_features=past_time_features,
+#     past_observed_mask=past_observed_mask,
+#     static_categorical_features=batch["static_categorical_features"],
+#     static_real_features=batch["static_real_features"],
+#     future_time_features=batch["future_time_features"],
+# )
 
-# 평균 예측값 계산
-mean_prediction = outputs.sequences.mean(dim=1)
+# # 평균 예측값 계산
+# mean_prediction = outputs.sequences.mean(dim=1)
 
-# 결과 출력
-print(mean_prediction)
+# # 결과 출력
+# print(mean_prediction)
