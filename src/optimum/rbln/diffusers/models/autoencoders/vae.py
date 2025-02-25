a21ec73590efdde325cd0d7f95f958a61914ec32
rebel-seinpark <seinpark@rebellions.ai>
2025-02-25 13:10:56 +0900

    bug fix

--- src/optimum/rbln/diffusers/models/autoencoders/vae.py
+++ src/optimum/rbln/diffusers/models/autoencoders/vae.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 
 import torch  # noqa: I001
 from diffusers import AutoencoderKL, AutoencoderKLTemporalDecoder
