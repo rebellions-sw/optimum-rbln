22f2487e3dad337ec442e031eccd956da13db348
rebel-dkhong <dk.hong@rebellions.ai>
2025-03-04 17:57:50 +0900

    fix: unused

--- src/optimum/rbln/diffusers/modeling_diffusers.py
+++ src/optimum/rbln/diffusers/modeling_diffusers.py
@@ -263,7 +263,7 @@ class RBLNDiffusionMixin:
             prefix = cls._prefix.get(connected_pipe_name, "")
             for submodule_name in connected_pipe_cls._submodules:
                 flattened_config[prefix + submodule_name] = connected_pipe_config[submodule_name]
-        flattened_config.update(prepared_config)
+        flattened_config.update(pipe_global_config)
         return flattened_config
 
     @classmethod
@@ -404,7 +404,6 @@ class RBLNDiffusionMixin:
             model.save_config(model_save_dir)
 
         if rbln_config.get("optimize_host_memory") is False:
-            breakpoint()
             # Keep compiled_model objs to further analysis. -> TODO: remove soon...
             model.compiled_models = []
             for name in cls._submodules:
