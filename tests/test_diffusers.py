4615a2feb158afa0f0eb32fdfcd0bc486583e8bf
rebel-seinpark <seinpark@rebellions.ai>
2025-03-07 16:52:39 +0900

    feat: tiny model

--- tests/test_diffusers.py
+++ tests/test_diffusers.py
@@ -233,48 +233,128 @@ class TestSVDImg2VidModel(BaseTest.TestModel):
     HF_MODEL_ID = "stabilityai/stable-video-diffusion-img2vid"
 
     GENERATION_KWARGS = {
-        "num_inference_steps": 1,
+        "num_inference_steps": 2,
         "generator": torch.manual_seed(42),
-        "image": to_pil_image(torch.randn(3, 64, 64, generator=torch.manual_seed(42)).clamp(0, 1)),
-        "num_frames": 14,
-        "decode_chunk_size": 7,
-    }
-    RBLN_CLASS_KWARGS = {
-        "rbln_img_width": 64,
-        "rbln_img_height": 64,
-        "num_frames": 14,
-        "decode_chunk_size": 7,
+        "image": to_pil_image(torch.randn(3, 32, 32, generator=torch.manual_seed(42)).clamp(0, 1)),
+        "num_frames": 2,
+        "decode_chunk_size": 2,
         "rbln_config": {
             "image_encoder": {"rbln_device": 0},
             "unet": {"rbln_device": 0},
             "vae": {"rbln_device": -1},
         },
     }
-
-
-class TestSVDXTImg2VidModel(BaseTest.TestModel):
-    RBLN_CLASS = RBLNStableVideoDiffusionPipeline
-    HF_MODEL_ID = "stabilityai/stable-video-diffusion-img2vid-xt"
-
-    GENERATION_KWARGS = {
-        "num_inference_steps": 1,
-        "generator": torch.manual_seed(42),
-        "image": to_pil_image(torch.randn(3, 64, 64, generator=torch.manual_seed(42)).clamp(0, 1)),
-        "num_frames": 25,
-        "decode_chunk_size": 5,
-    }
     RBLN_CLASS_KWARGS = {
-        "rbln_img_width": 64,
-        "rbln_img_height": 64,
-        "num_frames": 25,
-        "decode_chunk_size": 5,
+        "rbln_img_width": 32,
+        "rbln_img_height": 32,
+        "num_frames": 2,
+        "decode_chunk_size": 2,
         "rbln_config": {
             "image_encoder": {"rbln_device": 0},
             "unet": {"rbln_device": 0},
             "vae": {"rbln_device": -1},
         },
     }
+    
+    @classmethod
+    def get_dummy_components(cls):
+        from diffusers import (
+            AutoencoderKLTemporalDecoder,
+            EulerDiscreteScheduler,
+            UNetSpatioTemporalConditionModel,
+        )
+        from transformers import (
+            CLIPImageProcessor,
+            CLIPVisionConfig,
+            CLIPVisionModelWithProjection,
+        )
+        torch.manual_seed(0)
+        unet = UNetSpatioTemporalConditionModel(
+            block_out_channels=(32, 64),
+            layers_per_block=2,
+            sample_size=32,
+            in_channels=8,
+            out_channels=4,
+            down_block_types=(
+                "CrossAttnDownBlockSpatioTemporal",
+                "DownBlockSpatioTemporal",
+            ),
+            up_block_types=("UpBlockSpatioTemporal", "CrossAttnUpBlockSpatioTemporal"),
+            cross_attention_dim=32,
+            num_attention_heads=8,
+            projection_class_embeddings_input_dim=96,
+            addition_time_embed_dim=32,
+        )
+        scheduler = EulerDiscreteScheduler(
+            beta_start=0.00085,
+            beta_end=0.012,
+            beta_schedule="scaled_linear",
+            interpolation_type="linear",
+            num_train_timesteps=1000,
+            prediction_type="v_prediction",
+            sigma_max=700.0,
+            sigma_min=0.002,
+            steps_offset=1,
+            timestep_spacing="leading",
+            timestep_type="continuous",
+            trained_betas=None,
+            use_karras_sigmas=True,
+        )
 
+        torch.manual_seed(0)
+        vae = AutoencoderKLTemporalDecoder(
+            block_out_channels=[32, 64],
+            in_channels=3,
+            out_channels=3,
+            down_block_types=["DownEncoderBlock2D", "DownEncoderBlock2D"],
+            latent_channels=4,
+        )
+
+        torch.manual_seed(0)
+        config = CLIPVisionConfig(
+            hidden_size=32,
+            projection_dim=32,
+            num_hidden_layers=5,
+            num_attention_heads=4,
+            image_size=32,
+            intermediate_size=37,
+            patch_size=1,
+        )
+        image_encoder = CLIPVisionModelWithProjection(config)
+
+        torch.manual_seed(0)
+        feature_extractor = CLIPImageProcessor(crop_size=32, size=32)
+        components = {
+            "unet": unet,
+            "image_encoder": image_encoder,
+            "scheduler": scheduler,
+            "vae": vae,
+            "feature_extractor": feature_extractor,
+        }
+        return components
+    
+    @classmethod
+    def setUpClass(cls):
+        # import os
+        # import shutil
+        # # env_coverage = os.environ.get("OPTIMUM_RBLN_TEST_LEVEL", "default")
+        # # env_coverage = cls.TestLevel[env_coverage.upper()]
+        # if env_coverage.value < cls.TEST_LEVEL.value:
+        #     raise unittest.SkipTest(f"Skipped test : Test Coverage {env_coverage.name} < {cls.TEST_LEVEL.name}")
+
+        if os.path.exists(cls.get_rbln_local_dir()):
+            shutil.rmtree(cls.get_rbln_local_dir())
+
+        components = cls.get_dummy_components()
+        cls.model = cls.RBLN_CLASS.from_pretrained(
+            cls.HF_MODEL_ID,
+            export=True,
+            model_save_dir=cls.get_rbln_local_dir(),
+            rbln_device=cls.DEVICE,
+            **cls.RBLN_CLASS_KWARGS,
+            **cls.HF_CONFIG_KWARGS,
+            **components,
+        )
 
 if __name__ == "__main__":
     unittest.main()
