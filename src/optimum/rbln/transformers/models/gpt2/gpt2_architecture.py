ea9543101a4f0585394b7749b941b91da6ec2b20
thkim <thkim@rebellions.ai>
2025-03-07 17:09:05 +0900

    gpt2 fix

--- src/optimum/rbln/transformers/models/gpt2/gpt2_architecture.py
+++ src/optimum/rbln/transformers/models/gpt2/gpt2_architecture.py
@@ -37,7 +37,7 @@ class GPT2Wrapper(DecoderOnlyWrapper):
             raise NotImplementedError(f"flash attention ({self.attn_impl}) is not implemented for {self.__class__}")
         new_layers = []
         for layer in causal_lm.transformer.h:
-            new_self_attn = GPT2Attention(layer.attn)
+            new_self_attn = GPT2Attention(layer.attn, self.use_attention_mask)
             new_layer = GPT2Layer(layer, new_self_attn)
             new_layers.append(new_layer)
         new_model = GPT2Model(causal_lm.transformer, new_layers, max_seq_len=max_seq_len)
