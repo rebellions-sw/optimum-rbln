c2cbcf927c4c6129dc56b91805ed5d7d62777b57
rebel-seinpark <seinpark@rebellions.ai>
2025-03-10 19:51:14 +0900

    fix: rbln kwargs args

--- tests/test_diffusers.py
+++ tests/test_diffusers.py
@@ -242,8 +242,8 @@ class TestSVDImg2VidModel(BaseTest.TestModel):
     RBLN_CLASS_KWARGS = {
         "rbln_img_width": 64,
         "rbln_img_height": 64,
-        "num_frames": 14,
-        "decode_chunk_size": 7,
+        "rbln_num_frames": 14,
+        "rbln_decode_chunk_size": 1,
         "rbln_config": {
             "image_encoder": {"rbln_device": 0},
             "unet": {"rbln_device": 0},
@@ -261,13 +261,13 @@ class TestSVDXTImg2VidModel(BaseTest.TestModel):
         "generator": torch.manual_seed(42),
         "image": to_pil_image(torch.randn(3, 64, 64, generator=torch.manual_seed(42)).clamp(0, 1)),
         "num_frames": 25,
-        "decode_chunk_size": 5,
+        "decode_chunk_size": 1,
     }
     RBLN_CLASS_KWARGS = {
         "rbln_img_width": 64,
         "rbln_img_height": 64,
-        "num_frames": 25,
-        "decode_chunk_size": 5,
+        "rbln_num_frames": 25,
+        "rbln_decode_chunk_size": 5,
         "rbln_config": {
             "image_encoder": {"rbln_device": 0},
             "unet": {"rbln_device": 0},
