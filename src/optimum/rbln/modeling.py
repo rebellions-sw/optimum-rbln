f65296563f286a3eccb41eaa5a10dd12444f1624
rebel-jongho <jongho.choi@rebellions.ai>
2025-03-10 17:31:52 +0900

    revert modifications

--- src/optimum/rbln/modeling.py
+++ src/optimum/rbln/modeling.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
