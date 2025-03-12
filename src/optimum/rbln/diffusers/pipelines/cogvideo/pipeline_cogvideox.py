ec95acad2a4d7f2530d43ffd87154dd0426de0fd
rebel-seinpark <seinpark@rebellions.ai>
2025-03-12 15:25:55 +0900

    docs: add comment for version tracking

--- src/optimum/rbln/diffusers/pipelines/cogvideo/pipeline_cogvideox.py
+++ src/optimum/rbln/diffusers/pipelines/cogvideo/pipeline_cogvideox.py
@@ -4,6 +4,8 @@ from ...modeling_diffusers import RBLNDiffusionMixin
 
 
 class RBLNCogVideoXPipeline(RBLNDiffusionMixin, CogVideoXPipeline):
+    # NOTE: CovVideoX1.5 is supported from diffusers >= 0.32.0
+    # ref: https://github.com/huggingface/diffusers/blob/v0.32.0/src/diffusers/pipelines/cogvideo/pipeline_cogvideox.py
     original_class = CogVideoXPipeline
     # _submodules = ["text_encoder", "transformer", "vae"]
     _submodules = ["transformer"]
