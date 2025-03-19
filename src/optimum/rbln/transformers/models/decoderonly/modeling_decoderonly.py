a3906f57b4d310707a42b1eac1ee5d11e81e2dce
rebel-thkim <157466331+rebel-thkim@users.noreply.github.com>
2025-03-19 18:02:21 +0900

    Update src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
    Co-authored-by: Sungho Shin <87514200+rebel-shshin@users.noreply.github.com>

--- src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
+++ src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
@@ -97,9 +97,10 @@ class RBLNRuntimeModel(RBLNPytorchRuntime):
         """
 
         NO_BLOCKS_ERROR = (
-            "No available blocks for allocation. "
-            "The generate() API does not have enough memory blocks to complete this inference job. "
-            "Consider using vllm-rbln for better resource management."
+            "No available blocks for allocation."
+            "The generate() API does not have enough memory blocks to complete this inference task."
+            "The failure seems to be due to the need for Paged Attention, which is implemented in [`vllm-rbln`](https://docs.rbln.ai/software/model_serving/vllm_support/vllm-rbln.html)"
+            "Using `vllm-rbln` should resolve the issue without any problems."
         )
 
         def update_block(batch_idx: int, block_idx: int):
