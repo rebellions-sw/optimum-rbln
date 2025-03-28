8324e14bce24751f9d33a88c7aab55efe6e04452
thkim <thkim@rebellions.ai>
2025-03-28 12:20:25 +0900

    bart fix

--- src/optimum/rbln/transformers/models/seq2seq/modeling_seq2seq.py
+++ src/optimum/rbln/transformers/models/seq2seq/modeling_seq2seq.py
@@ -302,8 +302,6 @@ class RBLNModelForSeq2SeqLM(RBLNModel, ABC):
             ]
         )
 
-        if cls.support_paged_causal_attn:
-            dec_input_info.insert(3, ("block_tables", [rbln_batch_size, 1], "int16"))
         if rbln_use_attention_mask:
             dec_input_info.insert(1, ("attention_mask", [rbln_batch_size, rbln_dec_max_seq_len], "float32"))
 
