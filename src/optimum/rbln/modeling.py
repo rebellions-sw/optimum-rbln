93fd706055dd78f68df561bf8521df5e0eefe596
junam.song <junam.song@rebellions.ai>
2025-04-15 14:42:36 +0900

    update namming locally

--- src/optimum/rbln/modeling.py
+++ src/optimum/rbln/modeling.py
@@ -129,9 +129,9 @@ class RBLNModel(RBLNBaseModel):
             config_path = save_dir_path / subfolder / "generation_config.json"
 
             generation_config.save_pretrained(config_path.parent)
-            config = json.loads(config_path.read_text(encoding="utf-8"))
-            config["transformers_version"] = generation_config.transformers_version
-            config_path.write_text(json.dumps(config, indent=2) + "\n", encoding="utf-8")
+            config_local = json.loads(config_path.read_text(encoding="utf-8"))
+            config_local["transformers_version"] = generation_config.transformers_version
+            config_path.write_text(json.dumps(config_local, indent=2) + "\n", encoding="utf-8")
 
         if not isinstance(config, PretrainedConfig):  # diffusers config
             config = PretrainedConfig(**config)
