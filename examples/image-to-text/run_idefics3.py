4702a16ee392f068d4db6e0e5b4019bcecd34388
kblee <kblee@rebellions.ai>
2025-04-15 15:12:29 +0900

    [skip ci] examples

--- examples/image-to-text/run_idefics3.py
+++ examples/image-to-text/run_idefics3.py
@@ -2,11 +2,10 @@ import os
 import typing
 
 import fire
-from transformers.image_utils import load_image
-from transformers import AutoProcessor, AutoModelForVision2Seq
-from transformers import Idefics3VisionTransformer
-from optimum.rbln import RBLNIdefics3VisionTransformer, RBLNIdefics3ForConditionalGeneration
-# from optimum.rbln import RBLNIdefics3ForConditionalGeneration, RBLNIdefics3VisionTransformer
+from datasets import load_dataset
+from transformers import AutoProcessor
+
+from optimum.rbln import RBLNIdefics3ForConditionalGeneration
 
 
 def main(
@@ -16,15 +15,9 @@ def main(
     prompt: typing.Optional[str] = None,
     max_seq_len: typing.Optional[int] = None,
     tensor_parallel_size: typing.Optional[int] = 4,
-    num_text_only: typing.Optional[int] = None,
 ):
     processor = AutoProcessor.from_pretrained(model_id)
-    # base_model = AutoModelForVision2Seq.from_pretrained(model_id)
-    # # vision_model = Idefics3VisionTransformer._from_config(base_model.config.vision_config)
-    # tmp_model = RBLNIdefics3Connector.from_model(
-    #     base_model.model
-    # )
-    # import pdb; pdb.set_trace()
+
     if from_transformers:
         model = RBLNIdefics3ForConditionalGeneration.from_pretrained(
             model_id,
@@ -32,9 +25,10 @@ def main(
             rbln_config={
                 "model.text_model": {
                     "attn_impl": "flash_attn",
-                    "max_seq_len" : 131072,
-                    "use_inputs_embeds" : True,
-                    "tensor_parallel_size" : 4,
+                    "max_seq_len": max_seq_len,
+                    "use_inputs_embeds": True,
+                    "tensor_parallel_size": tensor_parallel_size,
+                    "batch_size": batch_size,
                 }
             },
         )
@@ -45,67 +39,28 @@ def main(
             export=False,
         )
 
-    messages = [
-        {
-            "role": "user",
-            "content": [
-                {"type": "image"},
-                {"type": "text", "text": "What do we see in this image?"},
-            ]
-        },
-        {
-            "role": "assistant",
-            "content": [
-                {"type": "text", "text": "In this image, we can see the city of New York, and more specifically the Statue of Liberty."},
-            ]
-        },
-        {
-            "role": "user",
-            "content": [
-                {"type": "image"},
-                {"type": "text", "text": "And how about this image?"},
-            ]
-        },       
-    ]
-    
-    image1 = load_image("https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg")
-    image2 = load_image("https://cdn.britannica.com/59/94459-050-DBA42467/Skyline-Chicago.jpg")
-    image3 = load_image("https://cdn.britannica.com/68/170868-050-8DDE8263/Golden-Gate-Bridge-San-Francisco.jpg")
-    
-    prompt = processor.apply_chat_template(messages, add_generation_prompt=True)
-    inputs = processor(text=prompt, images=[image1, image2], return_tensors="pt")
-    inputs = {k: v for k, v in inputs.items()}
-    
-    
-    # for vision
-    # vision_out = base_model.model.vision_model(inputs['pixel_values'].squeeze(0))
-    # print(vision_out.last_hidden_state)
-    # print('=============')
-    # rbln_out = model(inputs['pixel_values'].squeeze(0))
-    # print(rbln_out.last_hidden_state)
+    ds = load_dataset("HuggingFaceM4/the_cauldron", "ai2d", split="train")
+    samples = ds.select(range(batch_size))
+    images = []
+    prompts = []
 
-    # # compare
-    # import numpy as np
-    # from scipy import stats
-    # golden = vision_out.last_hidden_state.detach().numpy()
-    # rbln = rbln_out.last_hidden_state.detach().numpy()
-    # print(f"max l1 diff : {np.abs(golden - rbln).max()}")
-    # print(f"pearsonr : {stats.pearsonr(golden.flatten(), rbln.flatten()).statistic}")
-    
+    for sample in samples:
+        img = sample["images"]
+        images.append(img)
+
+        message = [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": "Describe this image."}]}]
+        prompt = processor.apply_chat_template(message, add_generation_prompt=True)
+        prompts.append(prompt)
+
+    inputs = processor(text=prompts, images=images, return_tensors="pt", padding=True)
+    inputs = {k: v for k, v in inputs.items()}
+    # Generate
+
     generated_ids = model.generate(**inputs, max_new_tokens=500)
     generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
 
-    print(generated_texts)
-    # output = model.generate(**inputs, max_new_tokens=200)
-
-    # for i in range(batch_size):
-    #     prompt_len = inputs.input_ids[i].shape[-1]
-    #     if i >= num_text_only:
-    #         images[i - num_text_only].save(f"batch_{i}.png", "png")
-    #     print(f"batch {i} -- Prompt --")
-    #     print(processor.decode(output[i][:prompt_len], skip_special_tokens=True))
-    #     print("---- Answer ----")
-    #     print(processor.decode(output[i][prompt_len:], skip_special_tokens=True))
+    for i, text in enumerate(generated_texts):
+        print(f"Sample {i + 1} generate:\n{text}\n")
 
 
 if __name__ == "__main__":
