ad6ef80f91e546e82432ee142048630ac0e2610f
rebel-dkhong <dk.hong@rebellions.ai>
2025-04-17 15:25:56 +0900

    model: support encoder for RBLNAutoencoderKLCosmos

--- src/optimum/rbln/diffusers/modeling_diffusers.py
+++ src/optimum/rbln/diffusers/modeling_diffusers.py
@@ -85,6 +85,10 @@ class RBLNDiffusionMixin:
     def is_inpaint_pipeline(cls):
         return "Inpaint" in cls.__name__
 
+    @classmethod
+    def is_video_pipeline(cls):
+        return "Video" in cls.__name__
+
     @staticmethod
     def _maybe_apply_and_fuse_lora(
         model: torch.nn.Module,
