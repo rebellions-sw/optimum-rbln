fcfa551e4a6d858a1ebbb6da54efe594c24566b3
thkim <thkim@rebellions.ai>
2025-04-18 18:21:44 +0900

    [skip ci] ruff

--- src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
+++ src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
@@ -259,7 +259,9 @@ class RBLNRuntimeModel(RBLNPytorchRuntime):
         # If an attention mask is provided, select only the valid (non-masked) inputs
         inputs = inputs[:, attention_mask.bool()] if attention_mask is not None else inputs
         if position_embed is not None:
-            position_embed = position_embed[:,:,:,attention_mask.bool(),:] if attention_mask is not None else position_embed
+            position_embed = (
+                position_embed[:, :, :, attention_mask.bool(), :] if attention_mask is not None else position_embed
+            )
 
         query_length = inputs.shape[1]
         if query_length > self.max_seq_len:
