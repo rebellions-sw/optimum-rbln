fc065cfef637d51bc9753fbf0af3df9c97e4726c
rebel-dkhong <dk.hong@rebellions.ai>
2025-04-24 15:09:03 +0900

    update to follow changes of rbln config

--- src/optimum/rbln/diffusers/models/autoencoders/autoencoder_kl_cosmos.py
+++ src/optimum/rbln/diffusers/models/autoencoders/autoencoder_kl_cosmos.py
@@ -12,26 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Dict, List, Union
 
 import rebel
 import torch
-from diffusers.models.autoencoders.autoencoder_kl_cosmos import CosmosCausalConv3d
+from diffusers.models.autoencoders.autoencoder_kl_cosmos import AutoencoderKLCosmos, CosmosCausalConv3d
 from diffusers.models.autoencoders.vae import DecoderOutput
 from diffusers.models.modeling_outputs import AutoencoderKLOutput
 from torch.nn import functional as F
 from transformers import PretrainedConfig
 
+from ....configuration_utils import RBLNCompileConfig
 from ....modeling import RBLNModel
-from ....modeling_config import DEFAULT_COMPILED_MODEL_NAME, RBLNCompileConfig, RBLNConfig
 from ....utils.logging import get_logger
-from ...modeling_diffusers import RBLNDiffusionMixin
-from .vae import RBLNRuntimeVAEDecoder, RBLNRuntimeVAEEncoder, _VAECosmosDecoder, _VAEEncoder
+from ...configurations import RBLNAutoencoderKLCosmosConfig
+from .vae import RBLNRuntimeVAEDecoder, _VAECosmosDecoder
 
 
 if TYPE_CHECKING:
     import torch
-    from transformers import AutoFeatureExtractor, AutoProcessor, AutoTokenizer, PretrainedConfig
+    from transformers import AutoFeatureExtractor, AutoProcessor, AutoTokenizer, PretrainedConfig, PreTrainedModel
+
+    from ...modeling_diffusers import RBLNDiffusionMixin, RBLNDiffusionMixinConfig
 
 logger = get_logger(__name__)
 
@@ -45,23 +47,23 @@ def replaced_forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
 
 
 class RBLNAutoencoderKLCosmos(RBLNModel):
-    config_name = "config.json"
+    auto_model_class = AutoencoderKLCosmos
     hf_library_name = "diffusers"
+    _rbln_config_class = RBLNAutoencoderKLCosmosConfig
 
     def __post_init__(self, **kwargs):
         super().__post_init__(**kwargs)
-        if self.rbln_config.model_cfg.get("vid2vid_pipeline"):
-            self.encoder = RBLNRuntimeVAEEncoder(runtime=self.model[0], main_input_name="x")
-            self.decoder = RBLNRuntimeVAEDecoder(runtime=self.model[1], main_input_name="z")
-        else:
-            self.decoder = RBLNRuntimeVAEDecoder(runtime=self.model[0], main_input_name="z")
 
-        height = self.rbln_config.model_cfg.get("height")
-        width = self.rbln_config.model_cfg.get("width")
-        self.image_size = [height, width]
+        if self.rbln_config.uses_encoder:
+            logger.warning("We currently support Text to World pipeline only.")
+            self.rbln_config.uses_encoder = False
+
+        self.encoder = None
+        self.decoder = RBLNRuntimeVAEDecoder(runtime=self.model[-1], main_input_name="z")
+        self.image_size = self.rbln_config.image_size
 
     @classmethod
-    def wrap_model_if_needed(cls, model: torch.nn.Module, rbln_config: RBLNConfig) -> torch.nn.Module:
+    def wrap_model_if_needed(cls, model: torch.nn.Module, rbln_config: RBLNAutoencoderKLCosmosConfig) -> torch.nn.Module:
         def replace_forward_func(model):
             for name, module in model.named_children():
                 if isinstance(module, CosmosCausalConv3d) and module.temporal_pad == 0:
@@ -70,19 +72,15 @@ class RBLNAutoencoderKLCosmos(RBLNModel):
                     replace_forward_func(module)
 
         replace_forward_func(model)
-        if rbln_config.model_cfg.get("vid2vid_pipeline"):
-            encoder_model = _VAEEncoder(model)
-            decoder_model = _VAECosmosDecoder(model)
-            encoder_model.eval()
-            decoder_model.eval()
-            return encoder_model, decoder_model
-        else:
-            decoder_model = _VAECosmosDecoder(model)
-            decoder_model.eval()
-            return decoder_model
+        if rbln_config.uses_encoder:
+            logger.warning("We currently support Text to World pipeline only.")
+            rbln_config.uses_encoder = False
+        decoder_model = _VAECosmosDecoder(model)
+        decoder_model.eval()
+        return decoder_model
 
     @classmethod
-    def get_compiled_model(cls, model, rbln_config: RBLNConfig):
+    def get_compiled_model(cls, model, rbln_config: RBLNAutoencoderKLCosmosConfig) -> Dict[str, rebel.RBLNCompiledModel]:
         def compile_encoder_decoder():
             encoder_model, decoder_model = cls.wrap_model_if_needed(model, rbln_config)
             enc_compiled_model = cls.compile(encoder_model, rbln_compile_config=rbln_config.compile_cfgs[0])
@@ -94,20 +92,16 @@ class RBLNAutoencoderKLCosmos(RBLNModel):
             dec_compiled_model = cls.compile(decoder_model, rbln_compile_config=rbln_config.compile_cfgs[0])
             return dec_compiled_model
 
-        if rbln_config.model_cfg.get("vid2vid_pipeline"):
-            return compile_encoder_decoder()
-        else:
-            return compile_decoder_only()
+        return compile_decoder_only()
 
     @classmethod
-    def update_rbln_config_using_pipe(cls, pipe: RBLNDiffusionMixin, rbln_config: Dict[str, Any]) -> Dict[str, Any]:
+    def update_rbln_config_using_pipe(
+        cls, pipe: RBLNDiffusionMixin, rbln_config: "RBLNDiffusionMixinConfig", submodule_name: str
+    ) -> "RBLNDiffusionMixinConfig":
         num_channel_latents = pipe.transformer.config.in_channels
-        num_frames = rbln_config.get("num_frames", 121)
-        num_latent_frames = (num_frames - 1) // pipe.vae_scale_factor_temporal + 1
-        height = rbln_config.get("height", 704)
-        latent_height = height // pipe.vae_scale_factor_spatial
-        width = rbln_config.get("width", 1280)
-        latent_width = width // pipe.vae_scale_factor_spatial
+        num_latent_frames = (rbln_config.num_frames - 1) // pipe.vae_scale_factor_temporal + 1
+        latent_height = rbln_config.height // pipe.vae_scale_factor_spatial
+        latent_width = rbln_config.width // pipe.vae_scale_factor_spatial
 
         rbln_config.update(
             {
@@ -120,118 +114,82 @@ class RBLNAutoencoderKLCosmos(RBLNModel):
         return rbln_config
 
     @classmethod
-    def _get_rbln_config(
+    def _update_rbln_config(
         cls,
         preprocessors: Union["AutoFeatureExtractor", "AutoProcessor", "AutoTokenizer"],
+        model: "PreTrainedModel",
         model_config: "PretrainedConfig",
-        rbln_kwargs: Dict[str, Any] = {},
-    ) -> RBLNConfig:
-        # Since the Cosmos VAE Decoder already requires approximately 7.9 GiB of memory,
-        # Optimum-rbln cannot execute this model on RBLN-CA12 when the batch size > 1.
-        # However, the Cosmos VAE Decoder propose batch slicing when the batch size is greater than 1,
-        # Optimum-rbln utilize this method by compiling with batch_size=1 to enable batch slicing.
-        rbln_batch_size = 1
-        rbln_kwargs.update({"batch_size": 1})
+        rbln_config: RBLNAutoencoderKLCosmosConfig,
+    ) -> RBLNAutoencoderKLCosmosConfig:
 
-        rbln_num_channel_latents = rbln_kwargs.get("num_channel_latents")
-        rbln_num_latent_frames = rbln_kwargs.get("num_latent_frames")
-        rbln_latent_height = rbln_kwargs.get("latent_height")
-        rbln_latent_width = rbln_kwargs.get("latent_width")
-
-        if rbln_kwargs.get("vid2vid_pipeline"):
-            rbln_in_channels = model_config.in_channels if hasattr(model_config, "in_channels") else 3
-            rbln_num_frames = rbln_kwargs.get("num_frames", 121)
-            rbln_height = rbln_kwargs.get("height", 704)
-            rbln_width = rbln_kwargs.get("width", 1280)
+        compile_cfgs = []
+        if rbln_config.uses_encoder:
+            rbln_config.in_channels = model_config.in_channels if hasattr(model_config, "in_channels") else 3
 
             vae_enc_input_info = [
                 (
                     "x",
                     [
-                        rbln_batch_size,
-                        rbln_in_channels,
-                        rbln_num_frames,
-                        rbln_height,
-                        rbln_width,
+                        rbln_config.batch_size,
+                        rbln_config.in_channels,
+                        rbln_config.num_frames,
+                        rbln_config.height,
+                        rbln_config.width,
                     ],
                     "float32",
                 ),
             ]
-            vae_dec_input_info = [
-                (
-                    "z",
-                    [
-                        rbln_batch_size,
-                        rbln_num_channel_latents,
-                        rbln_num_latent_frames,
-                        rbln_latent_height,
-                        rbln_latent_width,
-                    ],
-                    "float32",
-                ),
-            ]
-            enc_rbln_compile_config = RBLNCompileConfig(compiled_model_name="encoder", input_info=vae_enc_input_info)
-            dec_rbln_compile_config = RBLNCompileConfig(compiled_model_name="decoder", input_info=vae_dec_input_info)
+            compile_cfgs.append(RBLNCompileConfig(compiled_model_name="encoder", input_info=vae_enc_input_info))
 
-            compile_cfgs = [enc_rbln_compile_config, dec_rbln_compile_config]
-            rbln_config = RBLNConfig(
-                rbln_cls=cls.__name__,
-                compile_cfgs=compile_cfgs,
-                rbln_kwargs=rbln_kwargs,
-            )
-            return rbln_config
+        vae_dec_input_info = [
+            (
+                "z",
+                [
+                    rbln_config.batch_size,
+                    rbln_config.num_channel_latents,
+                    rbln_config.num_latent_frames,
+                    rbln_config.latent_height,
+                    rbln_config.latent_width,
+                ],
+                "float32",
+            ),
+        ]
+        compile_cfgs.append(RBLNCompileConfig(compiled_model_name="decoder", input_info=vae_dec_input_info))
 
-        else:
-            input_info = [
-                (
-                    "z",
-                    [
-                        rbln_batch_size,
-                        rbln_num_channel_latents,
-                        rbln_num_latent_frames,
-                        rbln_latent_height,
-                        rbln_latent_width,
-                    ],
-                    "float32",
-                )
-            ]
-            vae_config = RBLNCompileConfig(input_info=input_info)
-            rbln_config = RBLNConfig(
-                rbln_cls=cls.__name__,
-                compile_cfgs=[vae_config],
-                rbln_kwargs=rbln_kwargs,
-            )
-            return rbln_config
+        rbln_config.set_compile_cfgs(compile_cfgs)
+        return rbln_config
 
     @classmethod
     def _create_runtimes(
         cls,
         compiled_models: List[rebel.RBLNCompiledModel],
-        rbln_device_map: Dict[str, int],
-        activate_profiler: Optional[bool] = None,
+        rbln_config: RBLNAutoencoderKLCosmosConfig,
     ) -> List[rebel.Runtime]:
         if len(compiled_models) == 1:
-            if DEFAULT_COMPILED_MODEL_NAME not in rbln_device_map:
-                cls._raise_missing_compiled_file_error([DEFAULT_COMPILED_MODEL_NAME])
+            # decoder
+            expected_models = ["decoder"]
+        else:
+            # encoder, decoder
+            expected_models = ["encoder", "decoder"]
 
-            device_val = rbln_device_map[DEFAULT_COMPILED_MODEL_NAME]
-            return [
-                compiled_models[0].create_runtime(
-                    tensor_type="pt", device=device_val, activate_profiler=activate_profiler
-                )
-            ]
+        if any(model_name not in rbln_config.device_map for model_name in expected_models):
+            cls._raise_missing_compiled_file_error(expected_models)
 
-        if any(model_name not in rbln_device_map for model_name in ["encoder", "decoder"]):
-            cls._raise_missing_compiled_file_error(["encoder", "decoder"])
-
-        device_vals = [rbln_device_map["encoder"], rbln_device_map["decoder"]]
+        device_vals = [rbln_config.device_map[model_name] for model_name in expected_models]
         return [
-            compiled_model.create_runtime(tensor_type="pt", device=device_val, activate_profiler=activate_profiler)
+            rebel.Runtime(
+                compiled_model,
+                tensor_type="pt",
+                device=device_val,
+                activate_profiler=rbln_config.activate_profiler,
+            )
             for compiled_model, device_val in zip(compiled_models, device_vals)
         ]
 
-    def encode(self, x: torch.FloatTensor, **kwargs) -> torch.FloatTensor:
+    def encode(self, x: torch.FloatTensor, return_dict: bool = True, **kwargs) -> torch.FloatTensor:
         posterior = self.encoder.encode(x)
+        if not return_dict:
+            return (posterior,)
         return AutoencoderKLOutput(latent_dist=posterior)
 
     def _decode(self, z: torch.FloatTensor, return_dict: bool = True) -> torch.FloatTensor:
