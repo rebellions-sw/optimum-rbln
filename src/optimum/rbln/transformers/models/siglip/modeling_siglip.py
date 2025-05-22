cc9b2892e954bff438b643a674032477ab2fc516
thkim <thkim@rebellions.ai>
2025-05-23 00:42:12 +0900

    [skip ci] move up to image_size validation

--- src/optimum/rbln/transformers/models/siglip/modeling_siglip.py
+++ src/optimum/rbln/transformers/models/siglip/modeling_siglip.py
@@ -78,6 +78,8 @@ class RBLNSiglipVisionModel(RBLNModel):
 
         if isinstance(rbln_config.image_size, int):
             rbln_config.image_size = (rbln_config.image_size, rbln_config.image_size)
+        if rbln_config.image_size is None:
+            raise ValueError("`rbln_image_size` should be specified!")
 
         if rbln_config.interpolate_pos_encoding is None:
             logger.warning("interpolate_pos_encoding is not set, using False by default")
@@ -85,9 +87,6 @@ class RBLNSiglipVisionModel(RBLNModel):
 
         if rbln_config.output_hidden_states is None:
             rbln_config.output_hidden_states = model_config.output_hidden_states
-
-        if rbln_config.image_size is None:
-            raise ValueError("`rbln_image_size` should be specified!")
 
         rbln_compile_config = RBLNCompileConfig(
             input_info=[
