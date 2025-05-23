d2705624f1cfcff7ab621a9d5e8f35e0bd05580c
thkim <thkim@rebellions.ai>
2025-05-23 14:07:56 +0900

    Revert "get_maximum_num_blocks"
    This reverts commit d42639b28b93de35d8a7beb02aaf47be9fa19623.

--- src/optimum/rbln/transformers/models/gemma3/configuration_gemma3.py
+++ src/optimum/rbln/transformers/models/gemma3/configuration_gemma3.py
@@ -18,14 +18,12 @@ from ....configuration_utils import RBLNModelConfig
 from ..decoderonly.configuration_decoderonly import RBLNDecoderOnlyModelForCausalLMConfig
 from ..siglip.configuration_siglip import RBLNSiglipVisionModelConfig
 
-
 class RBLNGemma3ForCausalLMConfig(RBLNDecoderOnlyModelForCausalLMConfig):
     def __init__(self, use_position_ids: Optional[bool] = None, use_attention_mask: Optional[bool] = None, **kwargs):
         # use_attention_mask and use_position_ids are always True for Gemma3
         use_attention_mask = use_attention_mask or True
         use_position_ids = use_position_ids or True
         super().__init__(use_attention_mask=use_attention_mask, use_position_ids=use_position_ids, **kwargs)
-
 
 class RBLNGemma3ForConditionalGenerationConfig(RBLNModelConfig):
     submodules = ["vision_tower", "language_model"]
