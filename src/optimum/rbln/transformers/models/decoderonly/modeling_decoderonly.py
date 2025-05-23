678cc891ac3e55a1e0536d80dc64b50db2021143
thkim <thkim@rebellions.ai>
2025-05-23 10:40:04 +0900

    [skip ci] remove meanless comment

--- src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
+++ src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
@@ -827,10 +827,6 @@ class RBLNDecoderOnlyModelForCausalLM(RBLNModel):
         hidden_size: int,
         head_dim: int,
     ):
-        # Input order: Fixed inputs (input_ids_or_inputs_embeds, cache_position, block_tables),
-        # followed by conditional inputs (query_position, attention_mask, position_ids),
-        # and finally past_key_values. This aligns with `DecoderOnlyWrapper.prepare_forward_args`.
-
         if use_inputs_embeds:
             main_input = ("inputs_embeds", [batch_size, query_length, hidden_size], "float32")
         else:
