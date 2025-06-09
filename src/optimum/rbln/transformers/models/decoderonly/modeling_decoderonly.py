eb8e70281e19dcde2be09c8a7b0dd6f4a3baf66d
thkim <thkim@rebellions.ai>
2025-06-09 22:01:14 +0900

    [skip ci] docstring

--- src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
+++ src/optimum/rbln/transformers/models/decoderonly/modeling_decoderonly.py
@@ -838,6 +838,16 @@ class RBLNDecoderOnlyModelForCausalLM(RBLNModel):
         rbln_config: RBLNDecoderOnlyModelForCausalLMConfig,
         model_config: PretrainedConfig,
     ):
+        """
+        Returns the input information for the RBLNDecoderOnlyModel.
+
+        Args:
+            batch_size (int): The batch size.
+            query_length (int): The length of the query.
+            rbln_config (RBLNDecoderOnlyModelForCausalLMConfig): The RBLN model configuration.
+            model_config (PretrainedConfig): The HuggingFace model configuration.
+        """
+        
         num_attention_heads = getattr(model_config, "n_head", None) or getattr(model_config, "num_attention_heads")
         num_key_value_heads = getattr(model_config, "num_key_value_heads", None) or num_attention_heads
         num_hidden_layers = getattr(model_config, "n_layer", None) or getattr(model_config, "num_hidden_layers")
