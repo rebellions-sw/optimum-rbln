e9a48e550f01553b861d34faa43d3b1032765d6a
kblee <kblee@rebellions.ai>
2025-06-18 10:22:16 +0900

    [skip ci] fix gemma3

--- src/optimum/rbln/transformers/models/gemma3/modeling_gemma3.py
+++ src/optimum/rbln/transformers/models/gemma3/modeling_gemma3.py
@@ -126,6 +126,23 @@ class RBLNGemma3ForConditionalGeneration(RBLNModel):
 
     def can_generate(self):
         return True
+    
+    @classmethod
+    def get_pytorch_model(cls, *args, **kwargs):
+        model = super().get_pytorch_model(*args, **kwargs)
+        
+        with no_init_weights():
+            model_cls_name = model.model.language_model.__class__.__name__
+            causal_model_cls_name = model_cls_name.replace("TextModel", "ForCausalLM")
+            causal_model_cls = getattr(importlib.import_module("transformers"), causal_model_cls_name)
+            new_language_model= causal_model_cls(model.model.language_model.config)
+
+        new_language_model.lm_head = model.lm_head
+        new_language_model.model = model.model.language_model
+        model.model.language_model = new_language_model
+        model.lm_head = None
+        del model.lm_head
+        return model
 
     def __post_init__(self, **kwargs):
         self.vision_tower = LoopVisionTower(self.rbln_submodules[0])
