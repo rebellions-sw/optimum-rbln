4ddc00f8fea9436b75d187c10593086c11a28979
thkim <thkim@rebellions.ai>
2025-06-25 20:30:33 +0900

    ruff

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -348,8 +348,8 @@ class TestColPaliModel(BaseTest.TestModel):
     RBLN_CLASS = RBLNColPaliForRetrieval
     HF_MODEL_ID = "thkim93/colpali-hf-1layer"
     GENERATION_KWARGS = {
-        "input_ids": torch.full((1,1024),fill_value=257152,dtype=torch.int32),
-        "attention_mask": torch.ones((1,1024),dtype=torch.int32),
+        "input_ids": torch.full((1, 1024), fill_value=257152, dtype=torch.int32),
+        "attention_mask": torch.ones((1, 1024), dtype=torch.int32),
         "pixel_values": torch.randn(1, 3, 448, 448, generator=torch.manual_seed(42)),
     }
 
