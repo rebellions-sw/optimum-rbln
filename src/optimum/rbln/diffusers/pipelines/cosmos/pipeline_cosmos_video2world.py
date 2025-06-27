78abff04c72616a5de6094c5a4641a3a9f3dc218
rebel-jongho <jongho.choi@rebellions.ai>
2025-06-27 22:45:56 +0900

    fix type hint

--- src/optimum/rbln/diffusers/pipelines/cosmos/pipeline_cosmos_video2world.py
+++ src/optimum/rbln/diffusers/pipelines/cosmos/pipeline_cosmos_video2world.py
@@ -13,20 +13,19 @@
 # limitations under the License.
 
 
-from diffusers import CosmosVideoToWorldPipeline
-from diffusers.models import AutoencoderKLCosmos, CosmosTransformer3DModel
-from diffusers.schedulers import EDMEulerScheduler
-from transformers import T5EncoderModel, T5TokenizerFast
+from typing import Any, Dict, Optional
 
+from diffusers import CosmosVideoToWorldPipeline
+from diffusers.schedulers import EDMEulerScheduler
+from transformers import T5TokenizerFast
+
+from ....transformers.models.t5.modeling_t5 import RBLNT5EncoderModel
 from ....utils.logging import get_logger
 from ...modeling_diffusers import RBLNDiffusionMixin
+from ...models.autoencoders.autoencoder_kl_cosmos import RBLNAutoencoderKLCosmos
+from ...models.transformers.transformer_cosmos import RBLNCosmosTransformer3DModel
 from .cosmos_guardrail import RBLNCosmosSafetyChecker
 
-
-try:
-    from cosmos_guardrail import CosmosSafetyChecker
-except ImportError:
-    from .cosmos_guardrail import CosmosSafetyChecker
 
 logger = get_logger(__name__)
 
@@ -45,12 +44,12 @@ class RBLNCosmosVideoToWorldPipeline(RBLNDiffusionMixin, CosmosVideoToWorldPipel
 
     def __init__(
         self,
-        text_encoder: T5EncoderModel,
+        text_encoder: RBLNT5EncoderModel,
         tokenizer: T5TokenizerFast,
-        transformer: CosmosTransformer3DModel,
-        vae: AutoencoderKLCosmos,
+        transformer: RBLNCosmosTransformer3DModel,
+        vae: RBLNAutoencoderKLCosmos,
         scheduler: EDMEulerScheduler,
-        safety_checker: CosmosSafetyChecker = None,
+        safety_checker: RBLNCosmosSafetyChecker = None,
     ):
         if safety_checker is None:
             safety_checker = RBLNCosmosSafetyChecker()
@@ -79,3 +78,21 @@ class RBLNCosmosVideoToWorldPipeline(RBLNDiffusionMixin, CosmosVideoToWorldPipel
             )
             kwargs.pop("max_sequence_length")
         return kwargs
+
+    @classmethod
+    def from_pretrained(
+        cls,
+        model_id: str,
+        *,
+        export: bool = False,
+        safety_checker: Optional[RBLNCosmosSafetyChecker] = None,
+        rbln_config: Dict[str, Any] = {},
+        **kwargs: Dict[str, Any],
+    ):
+        rbln_config, kwargs = cls.get_rbln_config_class().initialize_from_kwargs(rbln_config, **kwargs)
+        if safety_checker is None and export:
+            safety_checker = RBLNCosmosSafetyChecker(rbln_config=rbln_config.safety_checker)
+
+        return super().from_pretrained(
+            model_id, export=export, safety_checker=safety_checker, rbln_config=rbln_config, **kwargs
+        )
