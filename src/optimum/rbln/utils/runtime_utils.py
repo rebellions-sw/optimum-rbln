59742abe17959ae643603b0d28e1ebeca0b74899
rebel-dkhong <dk.hong@rebellions.ai>
2025-07-09 10:22:41 +0900

    other: support runtime timeout (#209)
    Co-authored-by: rebel-jongho <112920593+rebel-jongho@users.noreply.github.com>

--- src/optimum/rbln/utils/runtime_utils.py
+++ src/optimum/rbln/utils/runtime_utils.py
@@ -163,13 +163,20 @@ class ContextRblnConfig:
     _local = threading.local()
 
     def __init__(
-        self, device=None, device_map=None, create_runtimes=None, optimize_host_mem=None, activate_profiler=None
+        self,
+        device=None,
+        device_map=None,
+        create_runtimes=None,
+        optimize_host_mem=None,
+        activate_profiler=None,
+        timeout=None,
     ):
         self.device = device
         self.device_map = device_map
         self.create_runtimes = create_runtimes
         self.optimize_host_mem = optimize_host_mem
         self.activate_profiler = activate_profiler
+        self.timeout = timeout
 
     def __enter__(self):
         self._local.device = self.device
@@ -177,6 +184,7 @@ class ContextRblnConfig:
         self._local.create_runtimes = self.create_runtimes
         self._local.optimize_host_memory = self.optimize_host_mem
         self._local.activate_profiler = self.activate_profiler
+        self._local.timeout = self.timeout
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -185,6 +193,7 @@ class ContextRblnConfig:
         self._local.create_runtimes = None
         self._local.optimize_host_memory = None
         self._local.activate_profiler = None
+        self._local.timeout = None
 
     @classmethod
     def get_current_context(cls):
@@ -194,4 +203,5 @@ class ContextRblnConfig:
             "create_runtimes": getattr(cls._local, "create_runtimes", None),
             "optimize_host_memory": getattr(cls._local, "optimize_host_memory", None),
             "activate_profiler": getattr(cls._local, "activate_profiler", None),
+            "timeout": getattr(cls._local, "timeout", None),
         }
