722514cf3019bcc7a0c42b80cd395e786c1a330d
rebel-thkim <157466331+rebel-thkim@users.noreply.github.com>
2025-08-08 16:32:09 +0900

    model: decoderonly runtime refactoring (#251)
    Co-authored-by: kblee <kblee@rebellions.ai>

---
+++ src/optimum/rbln/transformers/modeling_outputs.py
@@ -0,0 +1,37 @@
+# Copyright 2025 Rebellions Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import Optional, Tuple
+
+import torch
+from transformers.modeling_outputs import ModelOutput
+
+
+@dataclass
+class RBLNDecoderOnlyOutput(ModelOutput):
+    logits: torch.FloatTensor = None
+    generate_idx: torch.Tensor = None
+    padded_cache_lengths: int = None
+
+
+@dataclass
+class RBLNGemma3ForCausalLMOutput(RBLNDecoderOnlyOutput):
+    attention_mask: Optional[torch.Tensor] = None
+
+
+@dataclass
+class RBLNSeq2SeqTSDecoderOutput(ModelOutput):
+    last_hidden_states: torch.FloatTensor = None
+    params: Tuple[torch.FloatTensor] = None
--- src/optimum/rbln/transformers/models/decoderonly/generation_decoderonly.py
+++ src/optimum/rbln/transformers/models/decoderonly/generation_decoderonly.py
@@ -1,13 +1,25 @@
+# Copyright 2025 Rebellions Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import TYPE_CHECKING, Any, Dict, Optional
 
 import torch
-
-# from transformers import LlamaForCausalLM
 from transformers.generation.utils import GenerationMixin
 
 
 if TYPE_CHECKING:
-    from .modeling_decoderonly import RBLNDecoderOnlyForCausalLMOutput
+    from ...modeling_outputs import RBLNDecoderOnlyOutput
 
 
 class RBLNDecoderOnlyGenerationMixin(GenerationMixin):
@@ -68,7 +80,7 @@ class RBLNDecoderOnlyGenerationMixin(GenerationMixin):
         return model_inputs
 
     def _update_model_kwargs_for_generation(
-        self, outputs: "RBLNDecoderOnlyForCausalLMOutput", model_kwargs: Dict[str, Any], **kwargs
+        self, outputs: "RBLNDecoderOnlyOutput", model_kwargs: Dict[str, Any], **kwargs
     ) -> Dict[str, Any]:
         # update generate_idx
         model_kwargs["generate_idx"] = outputs.generate_idx
