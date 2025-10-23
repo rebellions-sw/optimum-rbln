d38a281ec8f72170cc22850bed0622e14d174ee2
seinpark <seinpark@rebellions.ai>
2025-10-23 14:01:07 +0900

    refactor: rm not used code

--- src/optimum/rbln/transformers/models/colqwen2/colqwen2_architecture.py
+++ src/optimum/rbln/transformers/models/colqwen2/colqwen2_architecture.py
@@ -41,18 +41,6 @@ def slice_and_unsqueeze_cos_sin(cos, sin, position_ids):
     return cos, sin
 
 
-def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
-    """
-    This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
-    num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
-    """
-    batch, num_key_value_heads, extra_dim, slen, head_dim = hidden_states.shape
-    if n_rep == 1:
-        return hidden_states
-    hidden_states = hidden_states.expand(batch, num_key_value_heads, n_rep, slen, head_dim)
-    return hidden_states.reshape(batch, num_key_value_heads * n_rep, 1, slen, head_dim)
-
-
 class ColQwen2LanguageModelWrapper(DecoderOnlyWrapper):
     def __init__(
         self, model: PreTrainedModel, rbln_config: "RBLNColQwen2ForRetrievalConfig", use_rotary_emb: bool = True
