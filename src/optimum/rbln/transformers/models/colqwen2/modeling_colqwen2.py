a6888b4dabeeacfb5d781dd7eab69b5d63208738
seinpark <seinpark@rebellions.ai>
2025-10-27 21:00:28 +0900

    make condition more robust

--- src/optimum/rbln/transformers/models/colqwen2/modeling_colqwen2.py
+++ src/optimum/rbln/transformers/models/colqwen2/modeling_colqwen2.py
@@ -71,7 +71,7 @@ class RBLNColQwen2ForRetrieval(RBLNDecoderOnlyModel):
         self.visual = self.rbln_submodules[0]
         self.prefill_runtime = self.model[0]
         self.mrope_section = self.config.text_config.rope_scaling["mrope_section"]
-        self.is_colqwen2_5 = self.config.model_type == "qwen2_5_vl"
+        self.is_colqwen2_5 = "qwen2_5_vl" in self.config.model_type
 
         if self.is_colqwen2_5:
             self.rotary_emb = Qwen2_5_VLRotaryEmbedding(self.config.text_config)
