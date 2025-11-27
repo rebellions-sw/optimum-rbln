e0bc1e973d1e95e0bc43409ef3f6213864254618
thkim <thkim@rebellions.ai>
2025-11-27 11:18:19 +0900

    fix xlm_roberta_model forward

--- src/optimum/rbln/transformers/models/xlm_roberta/modeling_xlm_roberta.py
+++ src/optimum/rbln/transformers/models/xlm_roberta/modeling_xlm_roberta.py
@@ -29,7 +29,6 @@ class RBLNXLMRobertaModel(RBLNTransformerEncoderForFeatureExtraction):
         self,
         input_ids: Optional[torch.Tensor] = None,
         attention_mask: Optional[torch.Tensor] = None,
-        token_type_ids: Optional[torch.Tensor] = None,
         **kwargs,
     ) -> Union[BaseModelOutputWithPoolingAndCrossAttentions, tuple]:
         """
@@ -38,13 +37,12 @@ class RBLNXLMRobertaModel(RBLNTransformerEncoderForFeatureExtraction):
         Args:
             input_ids (torch.Tensor of shape (batch_size, sequence_length), optional): Indices of input sequence tokens in the vocabulary.
             attention_mask (torch.Tensor of shape (batch_size, sequence_length), optional): Mask to avoid performing attention on padding token indices.
-            token_type_ids (torch.Tensor of shape (batch_size, sequence_length), optional): Segment token indices to indicate first and second portions of the inputs.
 
         Returns:
             The model outputs. If return_dict=False is passed, returns a tuple of tensors. Otherwise, returns a BaseModelOutputWithPoolingAndCrossAttentions object.
         """
 
-        return super().forward(input_ids, attention_mask, token_type_ids, **kwargs)
+        return super().forward(input_ids, attention_mask, **kwargs)
 
 
 class RBLNXLMRobertaForSequenceClassification(RBLNModelForSequenceClassification):
